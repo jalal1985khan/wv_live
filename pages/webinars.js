4790b95384e5038945dd87657ea9a7903f3c7118
jalal1985khan <jalal1985khan@gmail.com>
2023-11-17 22:15:45 +0530

    test-4

--- pages/webinars.js
+++ pages/webinars.js
@@ -13,7 +13,38 @@ const Webinars = ({ webinarvideo, spotlightvideo }) => {
   return (
   
     <>
-    Test
+  {
+
+webinarvideo.map((web, index) => {
+  return (
+    <Col md={4} className='  py-3' key={index}>
+      <Card className="webinar_post" >
+        <Image
+          src={web['_embedded']['wp:featuredmedia'][0]['source_url']}
+          alt={web['title']['rendered']}
+          className="img-hover webimg"
+          
+          onClick={() => {
+            setProduct(web.id)
+            setUrl(web.acf.video_url)
+            setTitle(web.title.rendered)
+          }}
+        />
+        <Card.Body>
+          <Card.Title className="fs-3 bogle-medium" style={{ height: 65 }} dangerouslySetInnerHTML={{ __html: web['title']['rendered'] }} />
+          <div dangerouslySetInnerHTML={{ __html: web['acf']['short_decription'] }} style={{ height: 180 }} />
+          <div style={{ height: 220 }}>
+            <Button variant="primary" className="pri-category mb-3" >{web['acf']['category']}</Button>
+            <h3 className="fs-5 bogle-medium mb-3">{web['acf']['expert_name']}</h3>
+            <h3 className="fs-6 mb-3">{web['acf']['expert_designation']}</h3>
+            <h3 className="fs-5 bogle-medium mb-3">{web['acf']['expert_name_copy']}</h3>
+            <h3 className="fs-6 mb-3">{web['acf']['expert_designation_copy']}</h3>
+          </div >
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+})}
     
     </>
 
