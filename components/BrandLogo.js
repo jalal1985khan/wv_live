2dd776246b0eab0dafedfd9d466c799dca7cc902
jalal1985khan <jalal1985khan@gmail.com>
2023-11-17 23:47:45 +0530

    test-6

--- components/BrandLogo.js
+++ components/BrandLogo.js
@@ -1,16 +1,18 @@
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import Image from 'next/image';
 function AutoLayoutSizingExample() {
   return (
     <Container fluid className="bg-light mx-auto p-3 border-primary border-bottom border-4 overflow-hidden">
       <Row>
         
         <Col sm={3} xs={6}>
-        <LazyLoadImage
-                  src='../images/brand-flipkart.png'
+        <Image
+                  src='/images/brand-flipkart.png'
                   alt=''
-                  
+            width={400}
+            height={200}
                   className="rounded mx-auto d-block img-fluid"
                   
                 />
@@ -18,10 +20,11 @@ function AutoLayoutSizingExample() {
         </Col>
         <Col className="m-tm-none" ></Col>
         <Col sm={3} xs={6}className='d-flex justify-content-end'>
-        <LazyLoadImage
-                  src='../images/brand-swasti.png'
+        <Image
+                  src='/images/brand-swasti.png'
                   alt=''
-                  
+                  width={400}
+                  height={200}
                   className="rounded mx-auto d-block img-fluid"
                   
                 />
