0528c31c36b04b3920168b8917c9420b4f59de7f
jalal1985khan <jalal1985khan@gmail.com>
2023-11-17 22:28:07 +0530

    test-6

--- pages/webinars.js
+++ pages/webinars.js
@@ -63,6 +63,143 @@ const Webinars = ({ webinarvideo, spotlightvideo }) => {
        
       />
       <Brand />
+      <Container fluid className="wbg-main p-0">
+        <Container className="text-center">
+          <p className="fs-1 bogle-medium text-white" >Webinars</p>
+          <Image src="/images/line-svg-png-1.png" width={100} height={20} alt="Industry Connect Series" />
+          <p className="fs-3 text-white">A collection of webinars that define the Walmart Vriddhi story through the MSME spotlight and industry connect series</p>
+        </Container>
+        <Container>
+          <div className="tabs">
+            <div className="tab-item active" >
+              <Image src="../images/settings.svg" alt="msme training Program online" className="img" />
+              <span className="eael-tab-title">Industry Connect Series</span>
+            </div>
+            <div className="tab-item">
+              <Image src="../images/peoples.svg" alt="msme training Program online" className="img" />
+              <span className="eael-tab-title">MSME Spotlight Series</span>
+            </div>
+
+            <div className="line"></div>
+          </div>
+        </Container>
+
+        <Container fluid className='wbg_grey'>
+          <div className="tab-content">
+            <div className="tab-pane active"  >
+              <Row className='w-center'>
+                <p className="fs-1 bogle-medium " >Webinars</p>
+                <Modal
+                  show={currentProduct}
+                  onHide={handleClose}
+                  closeTimeoutMS={300}
+                  isOpen={Boolean(currentProduct)}
+                  onRequestClose={() => setProduct(null)}
+                  size="lg"
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title dangerouslySetInnerHTML={{ __html: currentTitle }} />
+                  </Modal.Header>
+                  <Modal.Body><iframe width="100%" height="400" src={currentUrl} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe></Modal.Body>
+                </Modal>
+
+                {
+
+                  webinarvideo.map((web, index) => {
+                    return (
+                      <Col md={4} className='  py-3' key={index}>
+                        <Card className="webinar_post" >
+                          <Image
+                            src={web['_embedded']['wp:featuredmedia'][0]['source_url']}
+                            alt={web['title']['rendered']}
+                            className="img-hover webimg"
+                            
+                            onClick={() => {
+                              setProduct(web.id)
+                              setUrl(web.acf.video_url)
+                              setTitle(web.title.rendered)
+                            }}
+                          />
+                          <Card.Body>
+                            <Card.Title className="fs-3 bogle-medium" style={{ height: 65 }} dangerouslySetInnerHTML={{ __html: web['title']['rendered'] }} />
+                            <div dangerouslySetInnerHTML={{ __html: web['acf']['short_decription'] }} style={{ height: 180 }} />
+                            <div style={{ height: 220 }}>
+                              <Button variant="primary" className="pri-category mb-3" >{web['acf']['category']}</Button>
+                              <h3 className="fs-5 bogle-medium mb-3">{web['acf']['expert_name']}</h3>
+                              <h3 className="fs-6 mb-3">{web['acf']['expert_designation']}</h3>
+                              <h3 className="fs-5 bogle-medium mb-3">{web['acf']['expert_name_copy']}</h3>
+                              <h3 className="fs-6 mb-3">{web['acf']['expert_designation_copy']}</h3>
+                            </div >
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    )
+                  })}</Row>
+            </div>
+            {/* 2nd tab starts here  */}
+            <div className="tab-pane" >
+              <Row className='w-center'>
+                <p className="fs-1 bogle-medium " >Webinars</p>
+                <Modal show={currentProduct} onHide={handleClose}
+                  closeTimeoutMS={300}
+                  isOpen={Boolean(currentProduct)}
+                  onRequestClose={() => setProduct(null)}
+                  size="lg"
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title dangerouslySetInnerHTML={{ __html: currentTitle }} />
+                  </Modal.Header>
+                  <Modal.Body>
+                    <iframe width="100%" height="400" src={currentUrl} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe></Modal.Body>
+                </Modal>
+
+                {
+
+                  spotlightvideo.map((spot, index) => {
+
+                    return (
+
+                      <Col md={4} className='py-3' key={index}>
+
+                        <Card className="webinar_post" >
+                          <Image
+                            src={spot['_embedded']['wp:featuredmedia'][0]['source_url']}
+                            alt={spot['title']['rendered']}
+                            className="img-hover webimg"
+                           
+                            onClick={() => {
+                              setProduct(spot.id)
+                              setUrl(spot.acf.video_url)
+                              setTitle(spot.title.rendered)
+                            }}
+                          />
+                          <Card.Body>
+                            <Card.Title className="fs-3 bogle-medium" style={{ height: 65 }} dangerouslySetInnerHTML={{ __html: spot['title']['rendered'] }} />
+                            <div dangerouslySetInnerHTML={{ __html: spot['acf']['short_decription'] }} style={{ height: 180 }} />
+                            <div style={{ height: 220 }}>
+                              <Button variant="primary" className="pri-category mb-3" >{spot['acf']['category']}</Button>
+                              <h3 className="fs-5 bogle-medium mb-3">{spot['acf']['expert_name']}</h3>
+                              <h3 className="fs-6 mb-3">{spot['acf']['expert_designation']}</h3>
+                              <h3 className="fs-5 bogle-medium mb-3">{spot['acf']['expert_name_copy']}</h3>
+                              <h3 className="fs-6 mb-3">{spot['acf']['expert_designation_copy']}</h3>
+                            </div >
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    )
+
+
+                  })}
+              </Row>
+            </div>
+          </div>
+
+
+        </Container>
+
+
+      </Container>
+      <Footer />
     </>
   )
 }
