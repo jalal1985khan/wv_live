ab3d7f84b6f9fc446352e649594b3ed05e617c66
jalal1985khan <jalal1985khan@gmail.com>
2023-12-24 15:58:04 +0530

    final after qc

--- pages/terms-of-use.js
+++ pages/terms-of-use.js
@@ -1,83 +1,11 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { Container } from 'react-bootstrap';
-import configData from "../config.json";
-import { NextSeo } from 'next-seo';
-import {useRouter} from 'next/router';
+import React from 'react'
 
-
-const privacy = ({ data }) => {
-const pathname = useRouter()
-    return (
-        <div>
-    <NextSeo
-      title="Terms of use  - Walmart Vriddhi"
-      description="Terms of use  - Walmart Vriddhi"
-        canonical={pathname}
-        openGraph={{
-          url: pathname,
-          title: 'Terms of use  - Walmart Vriddhi',
-          description: 'Terms of use  - Walmart Vriddhi',
-          images: [
-            {
-              url:'/images/Walmart-Vriddhi-logo.svg',
-              width: 800,
-              height: 600,
-              alt: 'Walmart Vridhi',
-              type: 'image/jpeg',
-            },
-            {
-              url:'/images/Walmart-Vriddhi-logo.svg',
-              width: 900,
-              height: 800,
-              alt: 'Walmart Vridhi',
-              type: 'image/jpeg',
-            },
-            { url: '/images/Walmart-Vriddhi-logo.svg' },
-            { url: '/images/Walmart-Vriddhi-logo.svg' },
-          ],
-          siteName: "Terms of use  - Walmart Vriddhi",
-        }}
-        twitter={{
-          handle: '@handle',
-          site: '@site',
-          cardType: 'summary_large_image',
-        }}
-    />
-            <Header />
-            <Container fluid>
-                {
-                    data.map((post, index) => {
-                        //console.log(post)
-                        return (
-                          <div key={index}>
-                                <Container className="wbg-main d-flex justify-content-center align-items-center" style={{ height: 150 }}>
-                                    <h2 className="text-white bogle-medium">{post['title']['rendered']}</h2>
-                                </Container>
-                                <Container>
-                                    <div dangerouslySetInnerHTML={{ __html: post['acf']['terms_of_use'] }} className="fs-4" />
-
-                                </Container>
-                            </div>
-                        )
-
-
-                    })}
-            </Container>
-
-            <Footer />
-        </div>
-    )
+function termsUse() {
+  return (
+    <div>
+      hello
+    </div>
+  )
 }
 
-export default privacy
-
-
-export async function getServerSideProps(context) {
-    //const {id} = context.params;
-
-    const res = await fetch(`${configData.SERVER_URL}pages?_embed&slug=terms-of-use`);
-    const data = await res.json();
-    return { props: { data } }
-
-}
+export default termsUse
