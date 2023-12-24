8a889e2dc6b3900e0d015fa92fe9152bb8c6c9b8
jalal1985khan <jalal1985khan@gmail.com>
2023-12-24 16:01:15 +0530

    final after qc

--- pages/terms-of-use.js
+++ pages/terms-of-use.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Container } from 'react-bootstrap';
+import configData from "../config.json";
+import { NextSeo } from 'next-seo';
+import { usePathname } from 'next/navigation'
 
-function termsUse() {
+const privacy = ({ data }) => {  
+  const pathname = usePathname()
   return (
     <div>
-      hello
+      <NextSeo
+      title="Privacy policy  - Walmart Vriddhi"
+      description="Privacy policy  - Walmart Vriddhi"
+        canonical={pathname}
+        openGraph={{
+          url: pathname,
+          title: 'Privacy policy  - Walmart Vriddhi',
+          description: 'Privacy policy  - Walmart Vriddhi',
+          images: [
+            {
+              url:'/images/Walmart-Vriddhi-logo.svg',
+              width: 800,
+              height: 600,
+              alt: 'Walmart Vridhi',
+              type: 'image/jpeg',
+            },
+            {
+              url:'/images/Walmart-Vriddhi-logo.svg',
+              width: 900,
+              height: 800,
+              alt: 'Walmart Vridhi',
+              type: 'image/jpeg',
+            },
+            { url: '/images/Walmart-Vriddhi-logo.svg' },
+            { url: '/images/Walmart-Vriddhi-logo.svg' },
+          ],
+          siteName: "Privacy policy  - Walmart Vriddhi",
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+    />
+        <Header/>
+<Container fluid>
+{
+data.map((post,index)=>{
+console.log(post)
+return (
+<>
+<Container className="wbg-main d-flex justify-content-center align-items-center" style={{height:150}}>
+<h2 className="text-white bogle-medium">{post['title']['rendered']}</h2>
+</Container>
+<Container>
+<div dangerouslySetInnerHTML={{__html:post['acf']['privacy_policy']}} className="fs-4"/>
+
+</Container>
+</>
+ )
+
+
+})}
+</Container>
+
+      <Footer/>
     </div>
   )
 }
 
-export default termsUse
+export default privacy
+
+
+export async function getServerSideProps(context){
+    //const {id} = context.params;
+
+    const res = await fetch(`${configData.SERVER_URL}pages?_embed&slug=privacy-policy`);
+    const data = await res.json();
+    return {props:{data}}
+    
+    }
