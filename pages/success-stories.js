b5f7adcf34070b8cfbae3b9d0d2a1774167bf212
jalal1985khan <jalal1985khan@gmail.com>
2023-12-27 17:40:30 +0530

    final ok

--- pages/success-stories.js
+++ pages/success-stories.js
@@ -152,7 +152,7 @@ const SuccessStories = () => {
                       width="100%"
                     />
                     <Card.Body>
-                      <Button variant="primary" className="pri-category mb-3">MSME SuperPower: {post['acf']['primary_category']}</Button>
+                      <Button variant="primary" className="pri-category mb-3 bogle-medium">MSME SuperPower: {post['acf']['primary_category']}</Button>
                       <Card.Title className="fs-3 bogle-medium mb-4" style={{ height: 58 }} dangerouslySetInnerHTML={{__html:post['title']['rendered']}}/>
                       <h3 dangerouslySetInnerHTML={{ __html: post['acf']['author_name'] }} className="fs-4 authors bogle-medium"></h3>
                       <h3 dangerouslySetInnerHTML={{ __html: post['acf']['author_designation'] }} className="fs-7 mb-3" style={{ height: 25 }}></h3>
