ac23d5a8cec8f95bb412c7ee52dd0b9daf6b702c
jalal1985khan <jalal1985khan@gmail.com>
2024-01-02 16:49:16 +0530

    gtag updated

--- components/NewsLetter.js
+++ components/NewsLetter.js
@@ -1,6 +1,53 @@
+// NewsletterForm.js
+
 import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-//import sendEmail from './sendEmail'; // Import the sendEmail function
+
+const sendEmail = async (to, subject) => {
+  try {
+    // Set your SendGrid API key
+    const apiKey = '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'; // Replace with your SendGrid API key
+
+    // SendGrid API endpoint
+    const sendGridEndpoint = 'https://api.sendgrid.com/v3/mail/send';
+
+    // Create the email payload
+    const emailPayload = {
+      personalizations: [
+        {
+          to: [{ email: to }],
+          dynamic_template_data:'',
+        },
+      ],
+      from: { email: 'contactus@walmartvriddhi.org' }, // Replace with your email here
+      subject: subject,
+      template_id:'d-928effde4f674ad8b95ea8d8020051d5',
+    };
+
+    // Make the API request to SendGrid
+    const response = await fetch(sendGridEndpoint, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(emailPayload),
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      console.log('Email sent successfully:', responseData);
+      return responseData;
+    } else {
+      console.error('Error sending email:', responseData);
+      throw new Error('Failed to send email');
+    }
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
 
 const NewsletterForm = () => {
   const [email, setEmail] = useState('');
@@ -16,7 +63,6 @@ const NewsletterForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (email.trim() === '' || !isValidEmail) {
       setIsNotEmpty(email.trim() !== '');
       return;
@@ -24,11 +70,12 @@ const NewsletterForm = () => {
 
     try {
       // Save contact
+      const apiKey = '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'; // Replace with your SendGrid API key
       const response = await fetch('https://api.sendgrid.com/v3/marketing/contacts', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer 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`, // Replace with your SendGrid API key
+          Authorization: `Bearer ${apiKey}`, // Replace with your SendGrid API key
         },
         body: JSON.stringify({
           list_ids: ['ff61f48a-8aa5-4fc4-9d9d-3a8563f252f7'], // Replace with your SendGrid contact list ID
@@ -41,8 +88,7 @@ const NewsletterForm = () => {
         const templateId = 'd-928effde4f674ad8b95ea8d8020051d5'; // Replace with your SendGrid template ID
         const dynamicTemplateData = { /* Add your template variables here */ };
 
-        await sendEmail(email, 'Your Email Subject', templateId, dynamicTemplateData);
-        
+        await sendEmail(email, 'Your Email Subject', templateId);
         // Set state to indicate successful subscription
         setIsSent(true);
       } else {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,59 @@ import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Script from 'next/script';
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    
+        {/* Start Google Analytics Manager */}
+        <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-L833QYKDP3`}
+      />
+
+        <Script strategy="afterInteractive" id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-L833QYKDP3');
+        `}
+        </Script>
+        {/* End Google Analytics Manager */}
+
+
+        {/* Google Tag Manager (noscript) */}
+        <Script id="google-tag-manager-start">
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N899F6N');
+        `}
+      </Script>
+
+
+       <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N899F6N"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>  
+    <Component {...pageProps} />    
+    </>
+
+
+
+  )
 }
