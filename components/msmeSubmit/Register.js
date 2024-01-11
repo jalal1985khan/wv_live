84d12ba81ffc56b7edfa6e9d227e6066dc5c4d86
jalal1985khan <jalal1985khan@gmail.com>
2024-01-12 00:27:42 +0530

    msme 2024 page updated

--- components/msmeSubmit/Register.js
+++ components/msmeSubmit/Register.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 function Register() {
   return (
-    <Container className='text-center mt-5 mb-3 pt-5'>
+    <Container className='text-center mt-5 mb-3 pt-5 pb-5'>
     <Row>
         <Col>
         <Link href="https://forms.gle/1eZnYRz8qs1gxKST7" target='_blank' className='walmart-orange fs-5 bogle-medium text-white'>REGISTER NOW</Link>
--- pages/walmart-vriddhi-msme-summit-2024.js
+++ pages/walmart-vriddhi-msme-summit-2024.js
@@ -16,8 +16,48 @@ import Flipkart from '../components/msmeSubmit/InteractiveFlipkart'
 import Gallery from '../components/msmeSubmit/MsmeGallery'
 import Register from '../components/msmeSubmit/Register'
 function Summit() {
+
+  const title = "Walmart Vriddhi MSME Summit 2024: Unlocking Success with MSME Growth Program Benefits";
+  const desc = "Join us to discover unparalleled opportunities at the Walmart Vriddhi MSME Summit 2024. Unleash the potential of your business with insights from MSME growth program.";
+  const banner = '/images/msme.png';
+  const url = 'https://www.walmartvriddhi.org/walmart-vriddhi-msme-summit-2024';
+
   return (
     <>
+      <NextSeo
+      title= {title}
+      description={desc}
+        canonical={url}
+        openGraph={{
+          url: url,
+          title: title,
+          description: desc,
+          images: [
+            {
+              url: banner,
+              width: 800,
+              height: 600,
+              alt: 'Walmart Vridhi',
+              type: 'image/jpeg',
+            },
+            {
+              url: banner,
+              width: 900,
+              height: 800,
+              alt: 'Walmart Vridhi',
+              type: 'image/jpeg',
+            },
+            { url: banner },
+            { url: banner },
+          ],
+          siteName: title,
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+    />
           <Header />
           <Banner />
           <Brand />
