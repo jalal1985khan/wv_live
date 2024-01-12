065da17ea5e6cfe881956243adaeefcfab0765e6
jalal1985khan <jalal1985khan@gmail.com>
2024-01-12 17:58:11 +0530

    updated on landing page

--- components/msmeSubmit/Register.js
+++ components/msmeSubmit/Register.js
@@ -8,7 +8,7 @@ function Register() {
     <Row>
         <Col>
           <p className='mt-1 fs-5'>Don’t miss out on this opportunity to connect, learn, and grow.<br /> Click ‘Register Now’ to be a part of this inspiring journey.</p>
-          <br/>
+          <br/><br/>
           <Link href="https://forms.gle/1eZnYRz8qs1gxKST7" target='_blank' className='walmart-orange fs-5 bogle-medium text-white mt-5'>REGISTER NOW</Link>
         
         
