d2682a57b71f48679506b9cef70571420c69cfd4
jalal1985khan <jalal1985khan@gmail.com>
2024-01-12 17:54:06 +0530

    updated on landing page

--- components/msmeSubmit/Register.js
+++ components/msmeSubmit/Register.js
@@ -4,11 +4,13 @@ import Link from 'next/link'
 
 function Register() {
   return (
-    <Container className='text-center mt-5 mb-3 pt-5 pb-5'>
+    <Container className='text-center mt-5 mb-3 pt-1 pb-5'>
     <Row>
         <Col>
-        <Link href="https://forms.gle/1eZnYRz8qs1gxKST7" target='_blank' className='walmart-orange fs-5 bogle-medium text-white'>REGISTER NOW</Link>
-        <p className='mt-5 fs-5'>Don’t miss out on this opportunity to connect, learn, and grow.<br/> Click ‘Register Now’ to be a part of this inspiring journey.</p>
+          <p className='mt-1 fs-5'>Don’t miss out on this opportunity to connect, learn, and grow.<br /> Click ‘Register Now’ to be a part of this inspiring journey.</p>
+          <br/>
+          <Link href="https://forms.gle/1eZnYRz8qs1gxKST7" target='_blank' className='walmart-orange fs-5 bogle-medium text-white mt-5'>REGISTER NOW</Link>
+        
         
         </Col>
     </Row>
