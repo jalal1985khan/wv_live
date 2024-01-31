05a93a031a9e8ccca2341132927595bebefee450
jalal1985khan <jalal1985khan@gmail.com>
2024-01-31 11:29:04 +0530

    added 404 error custom design

--- pages/404.js
+++ pages/404.js
@@ -2,18 +2,25 @@ import React from 'react'
 import Header from '../components/Header';
 import Brand from '../components/BrandLogo';
 import Footer from '../components/Footer';
-import { Container,Row,Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import Link from 'next/link'
 
 function Error() {
   return (
     <div>
       <Header /> 
-      <Container>
+      <Container className='my-5'>
         <Row>
-          <Col>
-          
+        <Col sm={12} lg={5} className='text-center'>
+            <h1 className='bogle-bold walmart-default' style={{fontSize:'15em'}}>404</h1>
+          <h2 className='walmart-primary fs-1'>Page not found!</h2>
           </Col>
-          <Col></Col>
+          <Col sm={12} lg={7}>
+            <h2 className='fs-1 bogle-medium walmart-default'>Accept our appologies!</h2>
+            <p className='fs-3 mb-5'>The page you were looking for doesn't exists.You may have misstyped the address or the page may have moved.</p>
+            <Link href="" className='error-btn fs-5 text-white '>Go to Home page</Link>
+          </Col>
+          
 
 </Row>
 
