291348adeaf3ad4c6351ebdad1a1a951d832f09c
jalal1985khan <jalal1985khan@gmail.com>
2024-01-31 11:45:22 +0530

    added meta to 404 page

--- pages/404.js
+++ pages/404.js
@@ -4,10 +4,52 @@ import Brand from '../components/BrandLogo';
 import Footer from '../components/Footer';
 import { Container, Row, Col } from 'react-bootstrap';
 import Link from 'next/link'
+import { NextSeo } from 'next-seo';
+
+const title = "404 - page not found";
+const desc = "page not found";
+const banner = '/images/banner_about.jpeg';
+const url = 'https://www.walmartvriddhi.org/';
 
 function Error() {
   return (
     <div>
+      <NextSeo
+        noindex={true}
+        nofollow={true}
+      title= {title}
+      description={desc}
+        canonical={url}
+        openGraph={{
+          url: url,
+          title: title,
+          description: desc,
+          images: [
+            {
+              url: banner,
+              width: 800,
+              height: 600,
+              alt: 'Walmart Vridhi',
+              type: 'image/jpeg',
+            },
+            {
+              url: banner,
+              width: 900,
+              height: 800,
+              alt: 'Walmart Vridhi',
+              type: 'image/jpeg',
+            },
+            { url: banner },
+            { url: banner },
+          ],
+          siteName: title,
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+    />
       <Header /> 
       <Container className='my-5'>
         <Row>
